85ee48727a60d6b90d98a8b0e7549cc799d9580a
Danila <danila25golubev@gmail.com>
2025-09-19 13:22:03 +0300

    add changes return in currency and change readme for introduction and functions

--- app/api/endpoints/currency.py
+++ app/api/endpoints/currency.py
@@ -8,7 +8,7 @@ from app.api.schemas import Currency
 
 router = APIRouter()
 
-@router.get("/all", response_model=Dict[str, Currency], summary="Получить список всех валют")
+@router.get("/all", response_model=Dict[str, Currency], summary="Получить список всех валют с дополнительной информацией")
 async def all_currencies():
     try:
         return await get_api_all_currencies()
