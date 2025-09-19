357496a23f34fb701b09dc9bafef9d8d74f5211a
Danila <danila25golubev@gmail.com>
2025-09-19 20:46:07 +0300

    add is_verify_password to endpoint

--- app/api/endpoints/users.py
+++ app/api/endpoints/users.py
@@ -1,11 +1,12 @@
 from typing import Optional
 
 from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.api.schemas import UserRequest, UserDatabase
 from app.db.db_utils import get_user, add_new_user
-from sqlalchemy.ext.asyncio import AsyncSession
 from app.db import get_db
+from app.utils import is_verify_password
 
 
 router = APIRouter()
@@ -24,7 +25,8 @@ async def register(user: UserRequest, db: AsyncSession = Depends(get_db)):
 async def login(user: UserRequest, db: AsyncSession = Depends(get_db)):
     user_db: Optional[UserDatabase] = await get_user(db, user.username)
 
-    if user_db:
+    if user_db and is_verify_password(user.password, user_db.hashed_password):
+        
         return {"Сообщение": 'Пользователь успешно авторизирован'}
     else:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Неправильный пароль или никнейм')
