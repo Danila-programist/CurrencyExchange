354d69be0fa12b75c6c5adfc8c943b9d71e7fdd0
Danila <danila25golubev@gmail.com>
2025-09-19 18:57:44 +0300

    make dsn for postgresql

--- app/core/config.py
+++ app/core/config.py
@@ -1,5 +1,5 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
-from pydantic import AnyUrl
+from pydantic import AnyUrl, PostgresDsn
 
 class Settings(BaseSettings):
     
@@ -14,7 +14,19 @@ class Settings(BaseSettings):
     DB_PASSWORD: str
     DB_HOST: str
     DB_CONTAINER_NAME: str
+    DB_PORT: int
     DB: str
+
+    @property
+    def ASYNC_DATABASE_DSN(self) -> PostgresDsn:
+        return PostgresDsn.build(
+            scheme="postgresql+asyncpg",
+            user=self.DB_USER,
+            password=self.DB_PASSWORD,
+            host=self.DB_HOST,
+            port=str(self.DB_PORT),
+            path=f"/{self.DB_NAME}"
+        )
 
     model_config = SettingsConfigDict(env_file=['.env.example', '.env'], env_file_encoding='utf-8')
 
---
+++ app/db/models/users.py
@@ -0,0 +1,13 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String
+
+from models import Base
+
+class Users(Base):
+    __tablename__ = 'Users'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(String(16), nullable=False)
+    hashed_password: Mapped[str] = mapped_column(nullable=False)
+    role: Mapped[str] = mapped_column(nullable=False, server_default='user')
+    
