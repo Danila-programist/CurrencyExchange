3c1b3afc114ba074c695c642fd0c55c04a6a99b2
Danila <danila25golubev@gmail.com>
2025-09-19 21:17:45 +0300

    add readme for endpoints

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -4,4 +4,4 @@ from app.api.endpoints import currency, users
 api_router = APIRouter(prefix="/api/v1") 
 
 api_router.include_router(currency.router, prefix="/currency", tags=["Currency"])
-api_router.include_router(users.router, prefix="/users", tags=["users"])
+api_router.include_router(users.router, prefix="/users", tags=["Users"])
