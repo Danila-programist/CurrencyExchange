12127faf9e1abcc9914ebcefefc4c13d8ce7c6e3
Danila <danila25golubev@gmail.com>
2025-09-20 16:24:57 +0300

    change limiter

--- app/utils/limiter.py
+++ app/utils/limiter.py
@@ -1,15 +1,23 @@
 from typing import Dict
 
-from fastapi import Depends, HTTPException, status
+from fastapi import Depends, HTTPException, status, Request, Response
 from fastapi_limiter.depends import RateLimiter
 
 from app.core.security import get_role_from_token  
 
 def LimitChecker(limits: Dict[str, int]):
-    async def dependency(user_role: str = Depends(get_role_from_token)):
+    async def dependency(
+        request: Request,
+        response: Response,
+        user_role: str = Depends(get_role_from_token)
+    ):
         if not user_role:
             raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Неавторизован")
         if user_role not in limits:
             raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Лимитер не найден")
-        return RateLimiter(times=limits[user_role], minutes=1)
+
+        limiter = RateLimiter(times=limits[user_role], minutes=1)
+        await limiter(request, response)  
+        return user_role
+
     return dependency
